ac1bb88f8fc9e13b63418792f6e03c997561071e
Myoris <miriamyoris@hotmail.com>
2023-09-10 11:16:06 -0300

    generado route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import { Link } from "react-router-dom";
 
 
 function App() {
@@ -8,6 +14,20 @@ function App() {
       <div className="App">
           <Navbar/>
           <Footer/>
+          <BrowserRouter>
+              <Routes>
+                <Route>
+                  <nav>
+                      <Link to="/">Inicio</Link>
+                  </nav>
+                  <Route index element ={Home}/>
+                     <Route path="Contact" element ={<Contact/>}/>
+                     <Route path="Detail" element ={<Detail/>}/>
+                     <Route path="Favs" element ={<Favs/>}/>
+                </Route>
+              </Routes>
+          
+          </BrowserRouter>
       </div>
   );
 }
